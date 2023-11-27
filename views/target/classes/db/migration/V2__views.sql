04be179b84be1be498b286efaa0ef16f48a484c8
isacsebastiann <116098944+isacsebastiann@users.noreply.github.com>
2023-11-27 10:41:57 -0500

    first commit

---
+++ views/target/classes/db/migration/V2__views.sql
@@ -0,0 +1,11 @@
+/*CREATE VIEW IF NOT EXISTS invoice_view AS
+SELECT i.*, c.fullname FROM invoice i JOIN client c ON c.id = i.client_id;
+
+-- db/migration/V2__views.sql
+CREATE OR REPLACE VIEW invoice_view AS
+SELECT i.*, c.fullname
+FROM invoice i
+         JOIN client c ON c.id = i.client_id;*/
+/*REATE VIEW IF NOT EXISTS invoice_view AS
+SELECT i.*, c.fullname FROM invoice i JOIN client c ON c.id = i.client_id;
+
